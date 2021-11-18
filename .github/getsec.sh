ec4da900e1638d8bb4c9c4281535de69324a5f14
yordan-ng <yordan.golemanov@gmail.com>
2021-11-18 20:30:32 +0200

    test secrets

--- .github/getsec.sh
+++ .github/getsec.sh
@@ -6,8 +6,8 @@ env_filename="deployment.env"
 
 if [[ "${2,,}" == 'staging' ]]; then
 
-jq -r 'to_entries| map(select(.key | test("^SG_")?)) |map("\(.key[3:])=\(.value|tostring)")|.[]' <<< "$json_secrets_str"$
+jq -r 'to_entries| map(select(.key | test("^SG_")?)) |map("\(.key[3:])=\(.value|tostring)")|.[]' <<< "$json_secrets_str" > "$env_filename"
 elif [[ "${2,,}" == 'production' ]]; then
 
-jq -r 'to_entries| map(select(.key | test("^PR_")?)) |map("\(.key[3:])=\(.value|tostring)")|.[]' <<< "$json_secrets_str"$
+jq -r 'to_entries| map(select(.key | test("^PR_")?)) |map("\(.key[3:])=\(.value|tostring)")|.[]' <<< "$json_secrets_str" > "$env_filename"
 fi
